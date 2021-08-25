8590f01fcb8caa91ac31a36b070f523b609c5996
muk <76889639+human-ordinary@users.noreply.github.com>
2021-08-25 16:09:08 +0700

    Update videofun.py

--- userbot/modules/videofun.py
+++ userbot/modules/videofun.py
@@ -17,7 +17,7 @@ async def _(event):
         await event.edit("**Tidak bisa menemukan video asupan.**")
 
 
-@register(outgoing=True, pattern=r"^\.wibu$")
+@register(outgoing=True, pattern=r"^\.wibuu$")
 async def _(event):
     try:
         response = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
