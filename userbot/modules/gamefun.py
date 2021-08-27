29c603d6340dc1fac1403c6f62df59e50e53da8e
muk <76889639+human-ordinary@users.noreply.github.com>
2021-08-27 17:02:10 +0700

    Update gamefun.py

--- userbot/modules/gamefun.py
+++ userbot/modules/gamefun.py
@@ -65,9 +65,38 @@ async def _(event):
             await event.delete()
 
 
+@register(outgoing=True, pattern="^.karategame ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.pattern_match.group(1):
+        text, username = event.pattern_match.group(1).split()
+    else:
+        await event.delete()
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message("Play - Karate Kido 2")
+            await event.delete()
+            audio = await conv.get_response()
+            await event.delete()
+            audio = await conv.get_response()
+            await event.delete()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete()
+        except YouBlockedUserError:
+            await event.client(UnblockRequest("93372553"))
+            await conv.send_message("Play - Karate Kido 2")
+            await event.delete()
+            audio = await conv.get_response()
+            await event.delete()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete()
+
+
+
 CMD_HELP.update(
     {
-        "gamemuk": "`.ninjagame dan .racergame`\
+        "gamemuk": "`.ninjagame , .racergame dan .karategame`\
          \nNinja Game."
     }
 )
