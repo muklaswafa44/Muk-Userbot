18b8abe56be9b9bef1d2f4bb0d452fc5965e85f3
muk <76889639+human-ordinary@users.noreply.github.com>
2021-08-27 09:25:04 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -143,3 +143,193 @@ async def typewriter(typew):
 
 
 # Create by myself @localheart
+
+
+@register(outgoing=True, pattern="^.tiger(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`HA HAAA.....`")
+    sleep(1)
+    await typew.edit("`HARIIIMAAAUUUUU!!`")
+    sleep(1)
+    await typew.edit("`🏃                        🐅`")
+    await typew.edit("`🏃                       🐅`")
+    await typew.edit("`🏃                      🐅`")
+    await typew.edit("`🏃                     🐅`")
+    await typew.edit("`🏃   `LARII`          🐅`")
+    await typew.edit("`🏃                   🐅`")
+    await typew.edit("`🏃                  🐅`")
+    await typew.edit("`🏃                 🐅`")
+    await typew.edit("`🏃                🐅`")
+    await typew.edit("`🏃               🐅`")
+    await typew.edit("`🏃              🐅`")
+    await typew.edit("`🏃             🐅`")
+    await typew.edit("`🏃            🐅`")
+    await typew.edit("`🏃           🐅`")
+    await typew.edit("`🏃WOARGH!   🐅`")
+    await typew.edit("`🏃           🐅`")
+    await typew.edit("`🏃            🐅`")
+    await typew.edit("`🏃             🐅`")
+    await typew.edit("`🏃              🐅`")
+    await typew.edit("`🏃               🐅`")
+    await typew.edit("`🏃                🐅`")
+    await typew.edit("`🏃                 🐅`")
+    await typew.edit("`🏃                  🐅`")
+    await typew.edit("`🏃                   🐅`")
+    await typew.edit("`🏃                    🐅`")
+    await typew.edit("`🏃                     🐅`")
+    await typew.edit("`🏃  Huh-Huh           🐅`")
+    await typew.edit("`🏃                   🐅`")
+    await typew.edit("`🏃                  🐅`")
+    await typew.edit("`🏃                 🐅`")
+    await typew.edit("`🏃                🐅`")
+    await typew.edit("`🏃               🐅`")
+    await typew.edit("`🏃              🐅`")
+    await typew.edit("`🏃             🐅`")
+    await typew.edit("`🏃            🐅`")
+    await typew.edit("`🏃           🐅`")
+    await typew.edit("`🏃          🐅`")
+    await typew.edit("`🏃         🐅`")
+    await typew.edit("`DIA SEMAKIN MENDEKAT!!!`")
+    sleep(1)
+    await typew.edit("`🏃       🐅`")
+    await typew.edit("`🏃      🐅`")
+    await typew.edit("`🏃     🐅`")
+    await typew.edit("`🏃    🐅`")
+    await typew.edit("`Dahlah Pasrah Aja`")
+    sleep(1)
+    await typew.edit("`🧎🐅`")
+    sleep(2)
+    await typew.edit("`-TAMAT-`")
+
+
+@register(outgoing=True, pattern="^.halo(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n┻┳|―-∩`"
+                     "`\n┳┻|     ヽ`"
+                     "`\n┻┳|    ● |`"
+                     "`\n┳┻|▼) _ノ`"
+                     "`\n┻┳|￣  )`"
+                     "`\n┳ﾐ(￣ ／`"
+                     "`\n┻┳T￣|`"
+                     "\n**Hallo**")
+
+
+@register(outgoing=True, pattern="^.hoh(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(●_●)`"
+                     "`\n />❤️ *Ini Buat Kamu`")
+    sleep(3)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(●_●)`"
+                     "`\n/>💔  *eh patah`")
+    sleep(2)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(●_●)`"
+                     "`\n💔<\\  *aku ambil lagi deh`")
+
+
+@register(outgoing=True, pattern="^.noh(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(◕‿◕)`"
+                     "`\n />🌹 *Ini Buat Kamu`")
+    sleep(2)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(◡‿◡)`"
+                     "`\n🥀<\\  *Lah Layu, Gajadi maap🤣`")
+
+
+@register(outgoing=True, pattern="^.jawa(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("baik")
+    sleep(1)
+    await typew.edit("Tidak Sombong")
+    sleep(1)
+    await typew.edit("Ganteng")
+    sleep(1)
+    await typew.edit("Sopan")
+    sleep(1)
+    await typew.edit("Rajin")
+    sleep(1)
+    await typew.edit("Budiman")
+    sleep(1)
+    await typew.edit("Alim")
+    sleep(1)
+    await typew.edit("Berguna")
+    sleep(1)
+    await typew.edit("Nguli Juga")
+    sleep(1)
+    await typew.edit("Pemaaf")
+    sleep(1)
+    await typew.edit("Jujur")
+    sleep(1)
+    await typew.edit("Tidk Sombong")
+    sleep(1)
+    await typew.edit("Kaya")
+    sleep(1)
+    await typew.edit("Pokoknya Jawa Pro Dah")
+    sleep(1)
+    await typew.edit("Tidak Seperti Yang Lain")
+    sleep(1)
+    await typew.edit("Bersama Jawa Membangun Negri")
+
+
+@register(outgoing=True, pattern="^.gelay(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**Hai ,  Assalamualaikum**")
+    sleep(1)
+    await typew.edit("Kalian Nungguin aku gak??")
+    sleep(1)
+    await typew.edit("Ih ga mau🤢")
+    sleep(1)
+    await typew.edit("gasukaa😫")
+    sleep(1)
+    await typew.edit("__GELAYY__🤮")
+
+
+@register(outgoing=True, pattern="^.polisi(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("` 🏍️ `")
+    sleep(1)
+    await typew.edit("`ADA POLISI`")
+    sleep(1)
+    await typew.edit("` 🚓 `")
+    sleep(1)
+    await typew.edit("` WIU WIU`")
+    sleep(1)
+    await typew.edit("` 🚓 `")
+    sleep(2)
+    await typew.edit("`🚑`")
+    sleep(1)
+    await typew.edit("`🚓`")
+    sleep(1)
+    await typew.edit("`🚥 Yah Ketangkap`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern="^.tele(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("PC")
+    sleep(1)
+    await typew.edit("lanjut vc")
+    sleep(1)
+    await typew.edit("nyaman")
+    sleep(1)
+    await typew.edit("dighosting")
+    await typew.edit("siklus player tele")
+
+
+# Create by myself @localheart
