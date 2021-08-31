78a836449c5997c1667c216b3e043cf868de4d5b
muk <76889639+human-ordinary@users.noreply.github.com>
2021-08-31 20:20:58 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -109,7 +109,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/human-ordinary/Muk-Userbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Muk-Userbot")
+    "UPSTREAM_REPO_BRANCH", "Man-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
