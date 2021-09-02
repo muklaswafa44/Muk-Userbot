ae4a6b46a3f01f976d526d6441d39cfff7973488
muk <76889639+human-ordinary@users.noreply.github.com>
2021-09-02 16:29:49 +0700

    Create ownerbot.py

---
+++ userbot/modules/ownerbot.py
@@ -0,0 +1,46 @@
+from time import sleep
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.muk(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Hai Perkenalkan Namaku Muk`")
+    sleep(3)
+    await typew.edit("`20 Tahun`")
+    sleep(1)
+    await typew.edit("`Tinggal Di Tulungagung Jawa timur, Salam Kenal:)`")
+
+
+@register(outgoing=True, pattern='^.repobot(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("👻")
+    sleep(2)
+    await typew.edit("**🔥Ubot - Fork🔥**\n\n [Muk-Userbot](https://github.com/human-ordinary/Muk-Userbot)\n ")
+
+
+@register(outgoing=True, pattern='^.ownwerbot(?: |$)(.*)')  
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("👻")
+    sleep(2)
+    await typew.edit("**🔥Owner Bot🔥**\n\n [Muklas](https://t.me/iyainyabre)\n ")
+
+
+# muk
+
+CMD_HELP.update({
+    "owner":
+    "•CMD:`.muk`\
+    \nPenjelasan: perkenalan ownwer.\
+    \n\n•CMD:`.repobot`\
+    \nPenjelasan: link github owner.\
+    \n\n•CMD:`.owner`\
+    \nPenjelasan: telegram ownwer\
+    \n\n•CMD:`.fusage`\
